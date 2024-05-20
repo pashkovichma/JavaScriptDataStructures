d90bb672bc50afb055a8897f05a392b56fb014a8
Masha <masha@MacBook-Air.local>
2024-05-20 22:35:24 +0200

    linked list

---
+++ src/data-structures/linkedList.js
@@ -0,0 +1,51 @@
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Insert a node at the end
+  insrt(value) {
+    const newNode = new ListNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  // Delete a node by value
+  delete(value) {
+    if (this.head === null) return;
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      return;
+    }
+    let current = this.head;
+    while (current.next && current.next.value !== value) {
+      current = current.next;
+    }
+    if (current.next) {
+      current.next = current.next.next;
+    }
+  }
+
+  // Search for a node by value
+  search(value) {
+    let current = this.head;
+    while (current && current.value !== value) {
+      current = current.next;
+    }
+    return current;
+  }
+}
